71f2ef428fb1c38cdf0c1d13608173119cf42751
Colin McKean <colinmckeandigital@gmail.com>
2017-02-15 16:34:38 +0000

    initial commit

---
+++ wizard_management_specs/WizardTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import wizard_management.*;
+
+public class WizardTest {
+
+  Wizard wizard;
+  Broomstick broomstick;
+
+  @Before
+  public void before(){
+    broomstick = new Broomstick("Nimbus", 10);
+    wizard = new Wizard("Toby", broomstick);
+  }
+
+  @Test
+  public void hasName(){
+    assertEquals("Toby", wizard.getName());
+  }
+
+  @Test
+  public void hasBroomstick(){
+    Broomstick ride = (Broomstick)wizard.getRide();
+    assertEquals("Nimbus", ride.getBrand());
+  }
+
+  @Test
+  public void canFlyBroomStick(){
+    assertEquals(wizard.fly(),"mounting broom, running, skipping, flying!");
+  }
+  @Test
+  public void canFlyDragon(){
+    Dragon dragon = new Dragon("Smaug");
+    wizard  = new Wizard("Ridcully", dragon);
+    assertEquals("Standing up tall, beating wings, lift off!",wizard.fly());
+  }
+  @Test 
+  public void canFlyMagicCarpet(){
+    MagicCarpet magicCarpet = new MagicCarpet("Green");
+    wizard = new Wizard("CarpetRider", magicCarpet);
+    assertEquals("Hovering up, straightening out, flying off!", wizard.fly());
+  }
+  @Test
+  public void canSetRide(){
+    Dragon dragon = new Dragon("Erik");
+    wizard.setRide(dragon);
+    assertEquals("Standing up tall, beating wings, lift off!",wizard.fly());
+
+  }
+}
